e2d5622b2eefd63f40f8f39132ab9babed22f8c4
Francisco Eito <panchoeito@hotmail.com>
2023-03-20 18:07:47 -0300

    Agregando primer model 2

--- TerceraPreEntrega/Sala_de_armas/models.py
+++ TerceraPreEntrega/Sala_de_armas/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Usuarios(models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    dni = models.IntegerField()
+    
+    def __str__(self):
+        return f"{Usuarios.apellido} - {Usuarios.nombre} - {Usuarios.dni}"
+    
+    
